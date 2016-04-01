2a683f5443aa6635197d5aebd026184d5e681583
Rona <ronachong@gmail.com>
2016-03-31 18:01:13 -0700

    Update scriptvagrant.sh

--- scriptvagrant.sh
+++ scriptvagrant.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-ssh-keygen -t rsa -b 4096 -C "dalzuga@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "ronachong@gmail.com"
 cat ~/.ssh/id_rsa.pub
 #open https://github.com/settings/ssh
 
 echo 'PS1="\[\e[0;33m\]\h\[\e[m\]:\W \[\e[32m\]\u\[\e[m\]\[\e[31m\]\$\[\e[m\] "' >> ~/.bash_profile
 
-git config --global user.name "Daniel Alzugaray"
-git config --global user.email "dalzuga@gmail.com"
+git config --global user.name "Rona Chong"
+git config --global user.email "ronachong@gmail.com"
 git config --global core.editor emacs
 git config --global push.default simple
 git config --global alias.co checkout
